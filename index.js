834cbe78d42e1aa5bef3c09eebc9146fb71474dd
BrandonCMohammed <64518176+BrandonCMohammed@users.noreply.github.com>
2023-03-10 13:38:25 -0400

    learnt more things

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 console.log("Hello world!");
+
+//variables
+let name = 'Brandon';
+console.log(name);
+
+//const
+const interestRate = 0.1; 
+
+//objects
+ let person = {
+    name : 'Brandon',
+    age : 22
+ };
+
+ console.log(person);
+
+    //To access
+console.log(person.name);
+    //OR
+console.log(person['age']);
+
+//arrays
+let selectedColors = ['red', 'blue'];
+console.log(selectedColors)
+
+//functions
+
+function helloWorld(name, lname){
+    console.log("hello " + name + " " + lname +" from function!");
+
+}
+
+helloWorld("Brandon", "Mohmamed");
+
+function helloWorld2(name, lname){
+    return ("hello " + name + " " + lname +" from function 2!");
+
+}
+
+console.log(helloWorld2("Brandon", "Mohmamed"));
+
+//loops
+
+for (let i = 0; i < 10; i++){
+    console.log(i)
+
+};
+
+//If statements
+let x = 0;
+
+if (x ==0 ){
+    console.log("HI")
+} else if(x > 0){
+    console.log("HII")
+}else{
+    console.log("HII")
+}
